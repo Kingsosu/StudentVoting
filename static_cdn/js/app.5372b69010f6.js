af2ee41b97fdfa10eb23d29489682e9ade45cb78
Kingsosu <kingsosuolale@gmail.com>
2024-02-15 20:34:35 +0100

    second commit

---
+++ static_cdn/js/app.5372b69010f6.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var currentYear = new Date().getFullYear();
+    document.getElementById('footerDate').innerHTML = currentYear;
+  
+});
+
+
